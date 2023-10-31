24f7143f1ad4008ba77dd4737940cea4ba66b2d9
Katta <manibhargavi.katta@fiserv.com>
2023-10-31 14:37:38 -0400

    commit

---
+++ AH5/Solution.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+class Solution {
+    public static char mostFrequentChar(String s) {
+      Map<Character,Integer> hashMap = new HashMap(); 
+      
+      for(int i = 0; i< s.length(); i++){
+        char c = s.charAt(i);
+        if(hashMap.containsKey(c)) hashMap.replace(c,hashMap.get(c)+1);
+        else hashMap.put(c, 1);
+      }
+
+      int frequency = 0; 
+      char mostFreqentChar = 0;  
+      for(int i = 0; i< s.length(); i++){
+        if(hashMap.get(s.charAt(i))>frequency){
+          frequency = hashMap.get(s.charAt(i)); 
+          mostFreqentChar = s.charAt(i); 
+        }
+      }
+      return mostFreqentChar;
+    }
+  
+    public static void main(String[] args) {
+      // Try out your test cases:
+      System.out.println(Solution.mostFrequentChar("bookeeper")); // -> 'e'
+      System.out.println(Solution.mostFrequentChar("david")); // -> 'd'
+      System.out.println(Solution.mostFrequentChar("mississippi")); // -> 'i'
+      System.out.println(Solution.mostFrequentChar("eleventennine")); // -> 'e'
+    }
+
+  }
