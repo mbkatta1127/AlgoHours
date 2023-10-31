36d31fb60796b830dd6e9dcb86bf7211acb08e9d
Katta <manibhargavi.katta@fiserv.com>
2023-10-31 14:38:44 -0400

    commit

--- AH5/Solution.java
+++ AH5/Solution.java
@@ -5,7 +5,7 @@ import java.util.Set;
 
 class Solution {
     public static char mostFrequentChar(String s) {
-      Map<Character,Integer> hashMap = new HashMap(); 
+      Map<Character,Integer> hashMap = new HashMap();
       
       for(int i = 0; i< s.length(); i++){
         char c = s.charAt(i);
